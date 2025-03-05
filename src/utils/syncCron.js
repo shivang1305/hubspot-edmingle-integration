877b778e127faa78e2efcf1c113a442de550679a
Shivang Sharma <sharmashivang1999@gmail.com>
2025-03-05 22:46:59 +0530

    feat: cronjob code updated

--- src/utils/syncCron.js
+++ src/utils/syncCron.js
@@ -1,9 +1,21 @@
 const cron = require("node-cron");
-const { syncHubspotToEdmingle } = require("../controllers/sync.controller");
+const {
+  syncHubspotToEdmingle,
+  syncEdmingleToHubspot,
+} = require("../controllers/sync.controller");
+
+// ✅ Run the function immediately on startup
+(async () => {
+  console.log("⏳ Running First-Time Sync...");
+  // await syncHubSpotToEdmingle();
+  // await syncEdmingleToHubspot();
+  console.log("✅ First-Time Sync Completed!");
+})();
 
 cron.schedule("*/1 * * * *", async () => {
   console.log("⏳ Running HubSpot → Edmingle Sync...");
-  await syncHubspotToEdmingle();
+  //   await syncHubspotToEdmingle();
+  //   await syncHubspotToEdmingle();
   console.log("✅ Sync Completed!");
 });
 
